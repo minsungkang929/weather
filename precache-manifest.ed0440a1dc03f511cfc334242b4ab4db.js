12bc9b5d2bdfa644d754efef8dc486efd2f1f696
minsung kang <minsung.kang929@gmail.com>
2020-02-24 16:06:36 -0500

    Updates

---
+++ precache-manifest.ed0440a1dc03f511cfc334242b4ab4db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f80b0998a7a613dcdfb6255cb549979",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "c15307320d760815ddd8",
+    "url": "/weather/static/css/main.ada30426.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/weather/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "c15307320d760815ddd8",
+    "url": "/weather/static/js/main.b5f0bc1a.chunk.js"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  }
+]);
